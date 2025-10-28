ed1feae55050b98d84e3e9acea3344c997aee242
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2025-10-28 23:37:48 +0600

    Remove old directory structure
    Deleted obsolete directories:
    - src/application/
    - src/domain/
    - src/infrastructure/
    - src/presentation/
    
    All functionality has been moved to the new Clean Architecture structure:
    - src/core/
    - src/features/
    - src/shared/

--- src/presentation/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { LottieAnimation } from './LottieAnimation';
-export { UnderConstructionPage } from './UnderConstructionPage';
-export { HomePage } from './HomePage';
-export { Header } from './Header';
-export { LMSHomePage } from './LMSHomePage';
-export { CoursesPage } from './CoursesPage';
-export { LearningPage } from './LearningPage';
